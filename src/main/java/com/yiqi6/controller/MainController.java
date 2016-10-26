6e141cd81c45f9063abf7bf1a82ef8999daaf7c9
dengjihai <1454642611@qq.com>
2016-10-26 17:09:21 +0800

    1.完善web.xml 2.增加spring的xml配置文件

---
+++ src/main/java/com/yiqi6/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.yiqi6.controller;
+/**
+ * Created by Administrator on 2016-10-26.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 主页控制器
+ *
+ * @author dengjh
+ * @create 2016-10-26 17:01
+ **/
+@Controller
+public class MainController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<h2>看到了我，就说嘛配置成功！！</h2>
 </body>
 </html>
